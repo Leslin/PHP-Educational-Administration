f502df50dc9f85df0b68978d32b2368936576bab
dalin <li@cdhand.com>
2017-12-29 23:34:44 +0800

    组团

---
+++ application/index/controller/Group.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+use think\Request;
+use think\View;
+use think\Db;
+use think\Cache;
+use app\index\controller\Factory;
+use app\index\controller\Jssdk;
+
+/**
+ * group等
+ */
+class Group extends Controller {
+
+	public function index()
+	{	
+		return view();
+	}
+
+	public function creat()
+	{
+		return view();
+	}
+}
